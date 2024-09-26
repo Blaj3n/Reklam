3f46372ff65d0230f4cece31a4242c168ee8dcbd
Blaj3n <benceulrich@gmail.com>
2024-09-26 17:33:11 +0200

    2. feladat össz rendelés

--- reklam.py
+++ reklam.py
@@ -6,3 +6,10 @@ with open("rendel.txt", "r", encoding="utf-8") as file:
         rendel = rendel.strip().split()
         rendelesek.append([int(rendel[0]), str(rendel[1]), int(rendel[2])])
 print(rendelesek)
+
+print("2. feladat: ")
+# print(f"A rendelések száma: {len(rendelesek)}")
+ossz_rendeles = 0
+for rendel in rendelesek:
+    ossz_rendeles += 1
+print("A rendelések száma:", ossz_rendeles)
