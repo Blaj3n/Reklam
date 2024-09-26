6f17f7ba801a18e42b1f2ed40f07367fc8975cbf
Blaj3n <benceulrich@gmail.com>
2024-09-26 17:34:11 +0200

    3. feladat rendelés adott napon

--- reklam.py
+++ reklam.py
@@ -1,5 +1,4 @@
 # print("1. feladat")
-
 rendelesek = []
 with open("rendel.txt", "r", encoding="utf-8") as file:
     for rendel in file:
@@ -7,9 +6,19 @@ with open("rendel.txt", "r", encoding="utf-8") as file:
         rendelesek.append([int(rendel[0]), str(rendel[1]), int(rendel[2])])
 print(rendelesek)
 
+
 print("2. feladat: ")
 # print(f"A rendelések száma: {len(rendelesek)}")
 ossz_rendeles = 0
 for rendel in rendelesek:
     ossz_rendeles += 1
 print("A rendelések száma:", ossz_rendeles)
+
+
+print("3. feladat: ")
+megadott_nap = int(input("Kérem, adjon meg egy napot: "))
+adott_nap_csomagszam = 0
+for rendel in rendelesek:
+    if rendel[0] == megadott_nap:
+        adott_nap_csomagszam += 1
+print(f"A rendelések száma az adott napon: {adott_nap_csomagszam}")
