e068f3a3e69ee9076bb4b781e16211c67bdb5bad
Blaj3n <benceulrich@gmail.com>
2024-09-30 13:36:46 +0200

    4. feladat kész

--- reklam.py
+++ reklam.py
@@ -25,11 +25,47 @@ print(f"A rendelések száma az adott napon: {adott_nap_csomagszam}")
 
 
 # HÁZI: 7.-ig
-print("4. feladat: ")
+# print("4. feladat: ")
+# rendelt_nap = 0
+# nem_rendelt_nap = 0
+# for nap in range(1, 31):
+#     if rendel[1] == "NR":
+#         rendelt_nap += 1
+#     else:
+#         nem_rendelt_nap += 1
+# print(f"{nem_rendelt_nap} nap nem volt a reklámban nem érintett városból rendelés")
 
-nem_rendelt_nap = 0
-osszes_nap = range(1, 31)
-for nap in range(1, 31):
-    if rendel[1] == "NR" and rendel[2] == 0:
-        nem_rendelt_nap += 1
-print(f"{nem_rendelt_nap} nap nem volt a reklámban nem érintett városból rendelés")
+print("4. feladat: ")
+osszes_nap = []
+
+for nap in rendelesek:
+    if nap[0] not in osszes_nap:
+        osszes_nap.append(nap[0])
+print(osszes_nap)
+
+szures = []
+
+for nap in osszes_nap:
+    for rendel in rendelesek:
+        if rendel[0] == nap and rendel[1] == "NR" and nap not in szures:
+            szures.append(nap)
+print(szures)
+nincs_rendeles = len(osszes_nap) - len(szures)
+if len(osszes_nap) != len(szures):
+    print(f"{nincs_rendeles} nap nem volt a reklámban nem érintett városból rendelés")
+elif nincs_rendeles == 0:    # elif len(osszes_nap) == len(szures):
+    print("Minden nap volt rendelés a reklámban nem érintett városból")
+
+# print("5. feladat: ")
+#
+# legnagyobb_darabszam = 0
+# for rendel in rendelesek:
+#     if max(rendel[2]):
+#
+# print(f"A legnagyobb darabszám: {max(rendel[2])}, a rendelés napja: {rendel[0]}")
+
+# print("6. feladat")
+#
+#
+# def osszes(varos: str, sorszam: int):
+#     return varos, sorszam
