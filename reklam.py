ec213afe28754a747a2201da4dc8d86b7d01c4a8
Blaj3n <benceulrich@gmail.com>
2024-09-30 14:31:18 +0200

    házi

--- reklam.py
+++ reklam.py
@@ -17,7 +17,7 @@ print("A rendelések száma:", ossz_rendeles)
 
 print("3. feladat: ")
 
-megadott_nap = 3 #int(input("Kérem, adjon meg egy napot: "))
+megadott_nap = 3    # int(input("Kérem, adjon meg egy napot: "))
 
 adott_nap_csomagszam = 0
 for rendel in rendelesek:
@@ -25,17 +25,6 @@ for rendel in rendelesek:
         adott_nap_csomagszam += 1
 print(f"A rendelések száma az adott napon: {adott_nap_csomagszam}")
 
-
-# HÁZI: 7.-ig
-# print("4. feladat: ")
-# rendelt_nap = 0
-# nem_rendelt_nap = 0
-# for nap in range(1, 31):
-#     if rendel[1] == "NR":
-#         rendelt_nap += 1
-#     else:
-#         nem_rendelt_nap += 1
-# print(f"{nem_rendelt_nap} nap nem volt a reklámban nem érintett városból rendelés")
 
 print("4. feladat: ")
 osszes_nap = []
@@ -67,10 +56,13 @@ for rendel in rendelesek:
 # print(max(darabszamok))
 max_darabszam = max(darabszamok)
 # print(len(darabszamok), len(rendelesek))
-indexe_a_max_darabszam = darabszamok.index(max_darabszam)   # miután a len(darabszamok) == len(rendel.txt) [971], így ha veszem a darabszam lista maximumát [9], akkor simán megvizsgálhatom, hogy az hanyadik indexet foglalja el a darabszam listában.    # indexe_a_max_darabszam == 714
+indexe_a_max_darabszam = darabszamok.index(max_darabszam)   # miután a len(darabszamok) == len(rendel.txt) [971],
+# így ha veszem a darabszam lista maximumát [9], akkor simán megvizsgálhatom, hogy az hanyadik indexet foglalja el
+# a darabszam listában.    # indexe_a_max_darabszam == 714
 
 
-print(f"A legnagyobb darabszám: {rendelesek[indexe_a_max_darabszam][2]}, a rendelés napja: {rendelesek[indexe_a_max_darabszam][0]}")
+print(f"A legnagyobb darabszám: {rendelesek[indexe_a_max_darabszam][2]}, a rendelés napja:"
+      f"{rendelesek[indexe_a_max_darabszam][0]}")
 # print(indexe_a_max_darabszam)
 
 
@@ -89,3 +81,5 @@ osszes_TV_21 = osszes("TV", 21)
 osszes_NR_21 = osszes("NR", 21)
 print(f"7. feladat: \nA rendelt termékek darabszáma a 21. napon PL: {osszes_PL_21} TV: {osszes_TV_21}"
       f" NR: {osszes_NR_21}")
+
+# HF: 8. feladat
